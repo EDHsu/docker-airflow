134f9d0ac85aaa278484a5db5d1cd6b2b0318548
EDHsu <bassed1984@gmail.com>
2016-10-13 03:45:53 -0700

    test of python_operator

---
+++ example_dags/example_python_operator_test.sh
@@ -0,0 +1,5 @@
+echo "command layout: <airflow> <test> <dag_id> <task_id> <date>"
+
+airflow test example_python_operator print_the_context 2015-06-01
+
+airflow test example_python_operator sleep_for_5 2015-06-01
---
+++ example_dags/tutorial_test.sh
@@ -0,0 +1,5 @@
+echo "command layout: <airflow> <test> <dag_id> <task_id> <date>"
+
+airflow test tutorial print_date 2015-06-01
+
+airflow test tutorial sleep 2015-06-01
